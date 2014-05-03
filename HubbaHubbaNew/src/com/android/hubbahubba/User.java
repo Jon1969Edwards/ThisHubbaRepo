976976f51880cdc798d3438cdbc755770307b6e1
Robbie <robsmall@umich.edu>
2014-05-02 21:45:31 -0400

    back on real DB

--- HubbaHubbaNew/src/com/android/hubbahubba/User.java
+++ HubbaHubbaNew/src/com/android/hubbahubba/User.java
@@ -85,7 +85,7 @@ public class User {
 	public static final String PREFS_FILE = "com.android.hubbahubba.prefs";
 	
 	public static void loginToFacebook(Context context, String user_id, String access_token, int expire){
-		String url = IP + "/login/facebook";
+		String url = IPD + "/login/facebook";
 		String expire_string = Integer.toString(expire);
 		new FacebookLoginTask(context).execute(new String[] {url, user_id, access_token, expire_string}); 
 	}
